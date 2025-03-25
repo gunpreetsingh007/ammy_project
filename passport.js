35ceb5a7e2c06398e50ff2922b0a77193da353ef
Gunpreet Singh <alanwalker7172@gmail.com>
2025-03-26 00:07:00 +0530

    Restore Card Name step in passport.js for form submission

--- passport.js
+++ passport.js
@@ -139,16 +139,16 @@ const EMAIL_VALUE = 'gunpreetsinghking7172@gmail.com';
       //     await context.type('input[name="SingleLine1"]', APPLICATION_NO_VALUE);
       //   }
       // },
-      // {
-      //   name: 'Card Name',
-      //   selector: 'input[elname="stripeCardName"]',
-      //   completed: false,
-      //   fillInitially: true,
-      //   action: async (context) => {
-      //     await context.evaluate(selector => document.querySelector(selector).value = '', 'input[elname="stripeCardName"]');
-      //     await context.type('input[elname="stripeCardName"]', CARD_NAME_VALUE);
-      //   }
-      // },
+      {
+        name: 'Card Name',
+        selector: 'input[elname="stripeCardName"]',
+        completed: false,
+        fillInitially: true,
+        action: async (context) => {
+          await context.evaluate(selector => document.querySelector(selector).value = '', 'input[elname="stripeCardName"]');
+          await context.type('input[elname="stripeCardName"]', CARD_NAME_VALUE);
+        }
+      },
       {
         name: 'Card Number',
         selector: 'input[name="cardnumber"]',
